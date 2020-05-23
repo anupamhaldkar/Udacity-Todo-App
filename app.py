e383ea507b5f07076f5acfd3d8875da741613971
Anupam Haldkar <haldkaranupam2000@gmail.com>
2020-05-23 23:04:25 +0530

    Added List

--- app.py
+++ app.py
@@ -20,12 +20,18 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer,primary_key=True)
     description = db.Column(db.String(),nullable=False)
-    completed = db.Column(db.Boolean(),nullable=False,default=False)
-
+    completed = db.Column(db.Boolean(),default=False)
+    list_id = db.Column(db.Integer,db.ForeignKey('todolists.id'),nullable=False)
+    
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
 #db.create_all()
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id =db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(),nullable=False)
+    todos = db.relationship('Todo',backref='list',lazy=True)
 
 @app.route('/TodoApp/create', methods=['POST'])
 def create_todo():
@@ -47,8 +53,39 @@ def create_todo():
         abort(400)
     if not error:
         return jsonify(body)
-    
+
+@app.route('/todoApp/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))   
+
+@app.route('/todoApp/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({ 'success': True })
+
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html',
+    data=Todo.query.filter_by(list_id=list_id).order_by('id')
+    .all()
+    )
 
 @app.route('/')
 def index():
-    return render_template('index.html',data=Todo.query.all())
+    return redirect(url_for('get_list_todos',list_id=1))
